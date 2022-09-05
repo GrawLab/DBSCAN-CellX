bf6ce6bfb1dbab6758bc0624ce6c642951a55567
Pascal_Lukas <70589803+PasLukas@users.noreply.github.com>
2022-09-05 09:56:17 +0200

    Delete dbscan-cellx directory

--- dbscan-cellx/src/dbscan_cellx/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-
-
-# Version of the realpython-reader package
-__version__ = "0.0.1"
-
-# Read URL of the Real Python feed from config file
-#_cfg = tomllib.loads(resources.read_text("reader", "config.toml"))
-#URL = _cfg["feed"]["url"]
