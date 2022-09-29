f371db90ca6c38e4cf64315589fa0744f3a0d0da
Pascal_Lukas <70589803+PasLukas@users.noreply.github.com>
2022-09-29 13:11:25 +0200

    Add files via upload

--- dbscan-cellx/src/dbscan_cellx/dbscan_cellx.py
+++ dbscan-cellx/src/dbscan_cellx/dbscan_cellx.py
@@ -315,8 +315,8 @@ def edit_table(files, save, pixel_list, edge_mode, angel_paramter, save_paramter
             if edge_mode == 1:
                 table = edge(table, pixel_list, angel_paramter)
                 table = table.drop('Ind', axis=1)
-            name = j[:j.find(str("test_data_"))+11]
-
+            #name = j[:j.find(str("test_data_"))+11]
+            name = j[:-4]
             savepath =  name + '_DBSCAN_CELLX_output.csv'
             if save_paramter == 1:
                 savepath_para =  name + "_paramter_list.csv"
